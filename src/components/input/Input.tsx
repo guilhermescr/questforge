4b6578822b21d4c5e500145960137f993234c04a
guilhermescr <guilhermechaves2005@hotmail.com>
2025-11-25 02:35:55 -0300

    feat: enhance Input component to support disabled state and error handling

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -4,18 +4,35 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   error?: { message?: string };
+  state?: {
+    isChecked?: boolean;
+    isCorrect?: boolean;
+  };
 }
 
-export default function Input({ className, error, ...rest }: InputProps) {
+export default function Input({
+  className,
+  error,
+  state,
+  disabled,
+  ...rest
+}: InputProps) {
   return (
     <div className="w-full">
       <input
         {...rest}
         className={twMerge(
-          'w-full p-2 border-2 border-border rounded-md bg-input text-white focus:outline-none focus:ring focus:ring-ring focus:border-ring transition',
+          'w-full py-2 px-2.5 border-2 border-border rounded-md bg-input text-white focus:outline-none focus:ring focus:ring-ring focus:border-ring transition',
           error ? 'border-red-500' : '',
+          state?.isChecked
+            ? state.isCorrect
+              ? 'border-green-500'
+              : 'border-red-500'
+            : '',
+          disabled ? 'opacity-50 cursor-not-allowed' : '',
           className
         )}
+        disabled={disabled}
       />
       <ErrorMessage error={error} />
     </div>
