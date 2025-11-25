05ef4533e7075ae7351be9d46dd5c3fd7f1c3a9a
guilhermescr <guilhermechaves2005@hotmail.com>
2025-11-25 00:13:44 -0300

    feat: enhance layout component with public route check and pathname handling

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,29 +1,48 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import Header from '../../components/header/Header';
 import Wrapper from '@/src/components/wrapper/Wrapper';
-import routes from '@/src/lib/routes';
+import routes, { publicRoutes } from '@/src/lib/routes';
 import { useUserContext } from '@/src/context/UserContext';
 import Loading from '@/src/components/loading/Loading';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { user } = useUserContext();
   const router = useRouter();
+  const pathname = usePathname();
+
+  const isPublicRoute = (path: string) => {
+    return publicRoutes.some((route) => {
+      const routeSegments = route.split('/');
+      const pathSegments = path.split('/');
+
+      if (routeSegments.length !== pathSegments.length) {
+        return false;
+      }
+
+      return routeSegments.every((segment, index) => {
+        return segment === pathSegments[index] || segment === '[slug]';
+      });
+    });
+  };
 
   useEffect(() => {
-    if (user === undefined) return;
-    if (user === null) {
+    if (user === undefined) {
+      return;
+    }
+
+    if (!isPublicRoute(pathname) && user === null) {
       router.push(routes.auth);
     }
-  }, [user, router]);
+  }, [user, router, pathname]);
 
   if (user === undefined) {
     return <Loading height="min-h-screen" />;
   }
 
-  if (user === null) {
+  if (user === null && !isPublicRoute(pathname)) {
     return null;
   }
 
